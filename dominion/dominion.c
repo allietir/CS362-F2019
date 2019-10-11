19a7eacb972a4c9f7365cee5294a4471fb506f59
Alex Tir <tira@oregonstate.edu>
2019-10-11 15:41:19 -0700

    fixed bug in baronRefactor

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -689,24 +689,24 @@ int baronRefactor(int card, int choice1, struct gameState *state){
         int p = 0;//Iterator for hand!
         int card_not_discarded = 1;//Flag for discard set!
         while(card_not_discarded) {
-            if (state->hand[*currentPlayer][p] == estate) { //Found an estate card!
+            if (state->hand[currentPlayer][p] == estate) { //Found an estate card!
                 state->coins += 4;//Add 4 coins to the amount of coins
-                state->discard[*currentPlayer][state->discardCount[*currentPlayer]] = state->hand[*currentPlayer][p];
-                state->discardCount[*currentPlayer]++;
-                for (; p < state->handCount[*currentPlayer]; p++) {
-                    state->hand[*currentPlayer][p] = state->hand[*currentPlayer][p+1];
+                state->discard[currentPlayer][state->discardCount[currentPlayer]] = state->hand[currentPlayer][p];
+                state->discardCount[currentPlayer]++;
+                for (; p < state->handCount[currentPlayer]; p++) {
+                    state->hand[currentPlayer][p] = state->hand[currentPlayer][p+1];
                 }
-                state->hand[*currentPlayer][state->handCount[*currentPlayer]] = -1;
-                state->handCount[*currentPlayer]--;
+                state->hand[currentPlayer][state->handCount[currentPlayer]] = -1;
+                state->handCount[currentPlayer]--;
                 card_not_discarded = 0;//Exit the loop
             }
-            else if (p > state->handCount[*currentPlayer]) {
+            else if (p > state->handCount[currentPlayer]) {
                 if(DEBUG) {
                     printf("No estate cards in your hand, invalid choice\n");
                     printf("Must gain an estate if there are any\n");
                 }
                 if (supplyCount(estate, state) > 0) {
-                    gainCard(estate, state, 0, *currentPlayer);
+                    gainCard(estate, state, 0, currentPlayer);
 
                     state->supplyCount[estate]--;//Decrement estates
                     if (supplyCount(estate, state) == 0) {
@@ -724,7 +724,7 @@ int baronRefactor(int card, int choice1, struct gameState *state){
 
      else {
         if (supplyCount(estate, state) > 0) {
-            gainCard(estate, state, 0, *currentPlayer);//Gain an estate
+            gainCard(estate, state, 0, currentPlayer);//Gain an estate
 
             state->supplyCount[estate]--;//Decrement Estates
             if (supplyCount(estate, state) == 0) {
