7190f4ea5ea7713f61a642de5d6e258b4f792d72
Alex Tir <tira@oregonstate.edu>
2019-10-11 13:53:49 -0700

    changes

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -681,9 +681,6 @@ int getCost(int cardNumber)
     return -1;
 }
 
-int baron(){
-    return 0;
-}
 
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
