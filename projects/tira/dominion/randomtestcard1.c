3139c1809826bd9dc590fa8a3c6c1f8b9f3a9c37
Alex Tir <tira@oregonstate.edu>
2019-11-15 15:32:07 -0800

    updates

--- projects/tira/dominion/randomtestcard1.c
+++ projects/tira/dominion/randomtestcard1.c
@@ -8,6 +8,7 @@
 #include <time.h>
 
 #define TESTCARD "baron"
+#define NUMTESTS 1000
 
 int main() {
 	//seed rand
@@ -18,7 +19,7 @@ int main() {
 	int numFail = 0;
 	int testCount = 0;
 
-	int i;
+	int i, j;
 
 	//initialize relevant card variables
 	//most variables remain here for reusability, but unused variables are commented out
@@ -34,14 +35,21 @@ int main() {
 	int handPos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
 	
 	//variables for initializeGame()
-	int seed = 999;
-	int numPlayers = 2;
-	int currentPlayer = 0;
+	int seed;
+	int numPlayers;
+	int currentPlayer;
+
+	//decides if an estate will be placed
+	int placeEstate;
+
+	//flag for test oracle
+	int estateInHand;
 
 	//two game states used to compare card effects
 	struct gameState preState, postState;
 
 	//initialize card array
+	//i am deciding to keep the card array consistent for each test
 	int k[10] = {adventurer, minion, ambassador, gardens, mine, remodel, smithy, tribute, baron, great_hall};
 
 	//initialize a game using pregame state
@@ -49,11 +57,145 @@ int main() {
 
 	printf("----------------- Testing Card: %s -----------------\n\n", TESTCARD);
 
+	printf("THE FOLLOWING TESTS FAILED\n");
+	printf("TestNum\tnumPlayers\tcurrentPlayer\thandCount\testateInHand\tcardEffectChoice\tTestStatus");
+
 	//Randomize the gamestate
-	//randomize hand size
+	for (i = 0; i < NUMTESTS; i++)
+	{
+		//randomize 1-4 players
+		numPlayers = (rand() % 4) + 1;
+		//randomize seed
+		seed = rand() % 2000;
+		//randomize current player
+		currentPlayer = rand() % numPlayers;
+
+		initializeGame(numPlayers, k, seed, &preState);
+
+		//randomize deck size of current player
+		preState.deckCount[currentPlayer] = rand() % 30;
+
+		//randomize hand count, 1-5, so that there will always be at least 1 card in hand
+		//since playing baron is not possible if it was not in your hand
+		prestate.handCount[currentPlayer] = (rand() % 5) + 1;
+
+		//randomize position of estate card in hand
+		//also add a chance of no estate card in hand
+		placeEstate = rand() % (prestate.handCount[currentPlayer] + 1);
+		if (placeEstate < prestate.handCount[currentPlayer])
+		{
+			//place the estate
+			preState.hand[currentPlayer][placeEstate] = estate;
+			estateInHand = 1;
+		}
+		else //no estates
+		{
+			//get rid of any estates in hand
+			for ( j = 0; j < preState.handCount[currentPlayer]; j++)
+			{
+				if (postState.hand[currentPlayer][i] == estate)
+				{
+					postState.hand[currentPlayer][i] = mine;
+				}
+				estateInHand = 0;
+			}
+		}
+
+		//randomize player choice
+		choice1 = rand() % 2;
+		if (choice1 == 0)
+		{
+			//change choice variables based on card effects
+			discarded = 0;
+			netCoins = 0;
+			netSupply = -1;
+		}
+		else if (choice1 == 1)
+		{
+			//change choice variables based on card effects
+			discarded = 1;
+			netCoins = 4;
+			netSupply = 0;
+		}
+
+		//copy pregame state over to post game
+		memcpy(&postState, &preState, sizeof (struct gameState));
+
+		//call the card
+		cardEffect(baron, choice1, choice2, choice3, &postState, handPos, &bonus);
+
+
+
+		printf("Test: hand count = %d, expected = %d\tStatus: ", postState.handCount[currentPlayer], preState.handCount[currentPlayer] - discarded + drawnCards);
+		testCount++;
+		if (postState.handCount[currentPlayer] == preState.handCount[currentPlayer] - discarded + drawnCards)
+		{
+			numSuccess++;
+		}
+		else
+		{
+			//printf("TEST #%d FAILED: incorrect handcount\n", testCount);
+			//printf("TestNum\tnumPlayers\tcurrentPlayer\thandCount\testateInHand\tcardEffectChoice\tTestStatus");
+			printf("%d\t%d\t%d\t%d\t%d\t%d\tincorrect handcount", testCount, numPlayers, currentPlayer, preState.handCount[currentPlayer], estateInHand, choice1);
+			//printf("\tINPUTS:")
+		}
+		/*
+		printf("Test: deck count = %d, expected = %d\tStatus: ", postState.deckCount[currentPlayer], preState.deckCount[currentPlayer] - drawnCards + shuffledCards);
+		if (postState.deckCount[currentPlayer] == preState.deckCount[currentPlayer] - drawnCards + shuffledCards)
+		{
+			printf("SUCCESS\n");
+			numSuccess++;
+		}
+		else
+		{
+			printf("FAIL\n");
+			numFail++;
+		}
+
+		printf("Test: coins = %d, expected = %d\t\tStatus: ", postState.coins, preState.coins + netCoins);
+		if (postState.coins == preState.coins + netCoins)
+		{
+			printf("SUCCESS\n");
+			numSuccess++;
+		}
+		else
+		{
+			printf("FAIL\n");
+			numFail++;
+		}
+
+		printf("Test: buys = %d, expected = %d\t\tStatus: ", postState.numBuys, preState.numBuys + netBuys);
+		if (postState.numBuys == preState.numBuys + netBuys)
+		{
+			printf("SUCCESS\n");
+			numSuccess++;
+		}
+		else
+		{
+			printf("FAIL\n");
+			numFail++;
+		}
+
+		printf("Test: estate supply = %d, expected = %d\tStatus: ", postState.supplyCount[estate], preState.supplyCount[estate] + netSupply);
+		if (postState.supplyCount[estate] == preState.supplyCount[estate] + netSupply)
+		{
+			printf("SUCCESS\n");
+			numSuccess++;
+		}
+		else
+		{
+			printf("FAIL\n");
+			numFail++;
+		}
+
+		printf("\n");*/
+
+		//randomize hand size
 	//randomize library size
 	//randomize supply count
 	//randomize player card effect choice
+	}
+	/*
 
 	//*************************************************************************************************************
 	// TEST 1: choice1 = 1 = discard estate, +4 gold
@@ -294,10 +436,10 @@ int main() {
 		numFail++;
 	}
 
-	printf("\n");
+	printf("\n");*/
 
 	printf("Tests completed for %s.\n", TESTCARD);
-	printf("%d out of %d tests passed.\n\n", numSuccess, numSuccess + numFail);
+	printf("%d out of %d tests passed.\n\n", numSuccess, testCount);
 
 	return 0;
 }
