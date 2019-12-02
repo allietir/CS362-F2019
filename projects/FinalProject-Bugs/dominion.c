13fb8fb37b34014c2cad66813a9045fd36ca33cc
Alex Tir <tira@oregonstate.edu>
2019-12-02 11:27:38 -0800

    updates

--- projects/FinalProject-Bugs/dominion.c
+++ projects/FinalProject-Bugs/dominion.c
@@ -810,7 +810,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
         if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
         {
-            //return -1;
+            return -1;
         }
 
         if (choice2 > treasure_map || choice2 < curse)
@@ -820,7 +820,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
         if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
         {
-            return -1;
+           // return -1;
         }
 
         gainCard(choice2, state, 2, currentPlayer);
